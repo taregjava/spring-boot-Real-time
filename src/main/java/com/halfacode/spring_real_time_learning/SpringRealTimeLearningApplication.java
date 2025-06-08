b662ca90ef3a459d4fded2e2d21f018c0d9b6a5e
tareg <tareg.safi501@gmail.com>
2025-06-08 17:40:42 +0400

    Initial commit: base Spring Boot setup

---
+++ src/main/java/com/halfacode/spring_real_time_learning/SpringRealTimeLearningApplication.java
@@ -0,0 +1,13 @@
+package com.halfacode.spring_real_time_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRealTimeLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRealTimeLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/halfacode/spring_real_time_learning/SpringRealTimeLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halfacode.spring_real_time_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRealTimeLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
